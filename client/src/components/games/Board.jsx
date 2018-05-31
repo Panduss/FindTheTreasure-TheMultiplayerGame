bdd8af34028adceb4b84c592b8d30bb73b323b95
filippoimarisio <filippo.imarisio@gmail.com>
2018-05-31 13:22:58 +0200

    redesigned the board

--- client/src/components/games/Board.jsx
+++ client/src/components/games/Board.jsx
@@ -3,75 +3,20 @@ import './Board.css'
 
 
 export default (props) =>
-    <div>
-    <button
-      className="board-tile"
-      key={`1`}
-      onClick={(e) => props.makeMove(parseInt(e.target.innerHTML))}
-    >1</button>
-
-    <button
-      className="board-tile"
-      // disabled={hasTurn}
-      key={`2`}
-      onClick={(e) => props.makeMove(parseInt(e.target.innerHTML))}
-    >2</button>
-
-    <button
-      className="board-tile"
-      // disabled={hasTurn}
-      key={`3`}
-      onClick={(e) => props.makeMove(parseInt(e.target.innerHTML))}
-    >3</button>
-
-    <button
-      className="board-tile"
-      // disabled={hasTurn}
-      key={`4`}
-      onClick={(e) => props.makeMove(parseInt(e.target.innerHTML))}
-    >4</button>
-
-    <button
-      className="board-tile"
-      // disabled={hasTurn}
-      key={`5`}
-      onClick={(e) => props.makeMove(parseInt(e.target.innerHTML))}
-    >5</button>
-
-    <button
-      className="board-tile"
-      // disabled={hasTurn}
-      key={`6`}
-      onClick={(e) => props.makeMove(parseInt(e.target.innerHTML))}
-    >6</button>
-
-    <button
-      className="board-tile"
-      // disabled={hasTurn}
-      key={`7`}
-      onClick={(e) => props.makeMove(parseInt(e.target.innerHTML))}
-    >7</button>
-
-    <button
-      className="board-tile"
-      // disabled={hasTurn}
-      key={`8`}
-      onClick={(e) => props.makeMove(parseInt(e.target.innerHTML))}
-    >8</button>
-
-    <button
-      className="board-tile"
-      // disabled={hasTurn}
-      key={`9`}
-      onClick={(e) => props.makeMove(parseInt(e.target.innerHTML))}
-    >9</button>
-
-    </div>
-
-
-
-// export default ({board, makeMove}) => board.map((cells, rowIndex) =>
-//   <div key={rowIndex}>
-//     {cells.map((symbol, cellIndex) => renderCel(makeMove, rowIndex, cellIndex,symbol,false))}
-//   </div>
-// )
+<div>
+  <div>
+    <button className="board-tile" onClick={(event) => props.makeMove(event.target.innerHTML)}>1</button>
+    <button className="board-tile" onClick={(event) => props.makeMove(event.target.innerHTML)}>2</button>
+    <button className="board-tile" onClick={(event) => props.makeMove(event.target.innerHTML)}>3</button>
+  </div>
+  <div>
+    <button className="board-tile" onClick={(event) => props.makeMove(event.target.innerHTML)}>4</button>
+    <button className="board-tile" onClick={(event) => props.makeMove(event.target.innerHTML)}>5</button>
+    <button className="board-tile" onClick={(event) => props.makeMove(event.target.innerHTML)}>6</button>
+  </div>
+  <div>
+    <button className="board-tile" onClick={(event) => props.makeMove(event.target.innerHTML)}>7</button>
+    <button className="board-tile" onClick={(event) => props.makeMove(event.target.innerHTML)}>8</button>
+    <button className="board-tile" onClick={(event) => props.makeMove(event.target.innerHTML)}>9</button>
+  </div>
+</div>
